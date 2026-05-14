413d6d45b3d09643628166e67a2cccdbd8df3857
Savannah001 <dansonk899@gmail.com>
2026-05-14 23:11:29 +0300

    Js date formating and calculating nu,ber of nights btwn dates

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ var STORAGE_KEY = 'amboseli_bookings';
 function generateId() {
   return 'bk_' + Date.now().toString(36) + Math.random().toString(36).substring(2, 8);
 }
+// Format a date string
+function formatDate(dateStr) {
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(dateStr + 'T00:00:00').toLocaleDateString('en-US', options);
+}
+// Calculating number of nights between two dates
+function calculateNights(checkIn, checkOut) {
+  var start = new Date(checkIn);
+  var end = new Date(checkOut);
+  var diff = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
+  return diff > 0 ? diff : 0;
+}
